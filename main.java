1d966db8f7b2eb8d70a201a4e44577b291fa6dd0
Angelo Sandel Baesa <114591948+uzumakisandel@users.noreply.github.com>
2023-05-16 09:55:52 +0800

    Create main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+
+package personalinfojava;
+
+
+public class PersonalinfoJava {
+
+   
+    public static void main(String[] args) {
+        System.out.println("First Name;Angelo Sandel");
+        System.out.println("Last Name;Baesa");
+        System.out.println("Address;San Nicolas Baao CamSur");
+    }
+    
+}
